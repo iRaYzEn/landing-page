bcb29e72907f147925859589a165f71c579d0176
iRaYzEn <79583015+iRaYzEn@users.noreply.github.com>
2025-02-02 22:46:44 +0300

    implementing footer + improving Responsibility

--- README.md
+++ README.md
@@ -3,17 +3,30 @@ This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-
 ## front-end mentor project
 - https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5
 
-first 40 min:
+1st 40 min:
 - thinking of how to aproach desgin 
 - setting up the html for the page
 - styling the header
 - handling responsive desgin for the header
 - making couple of classes for use across the project
 
-second 40 min:
+2nd 40 min:
 - imporving mobile devices screen
 - coding the "what's different" page list
 - making couple of classes to use them across the project
 - improving the intro section
 
 ### end of the day 1, 80min of work
+
+3rd 40 min:
+- importing the font 
+- using the font weight across the elements
+- improving text Responsibility
+
+4th 40 min:
+- implementing the footer of the page without Responsibility
+- styling the footer
+
+### end of the day 2, 80min of work
+
+## total 120min
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,36 @@
 import Image from "next/image";
+import logo from "@/public/images/logo.svg"
+import facebook from "@/public/images/icon-facebook.svg"
+import instagram from "@/public/images/icon-instagram.svg"
+import twitter from "@/public/images/icon-twitter.svg"
+import pinterest from "@/public/images/icon-pinterest.svg"
+import youtube from "@/public/images/icon-youtube.svg"
 import heroImage from "@/public/images/illustration-intro.svg"
+
+import Link from "next/link";
+
 export default function Home() {
     return (
         <>
             <section className="section-gap">
-                <div className="wrapper flex items-center flex-wrap-reverse lg:flex-nowrap">
-                    <div className="text-center md:text-left">
-                        <h1 className="text-dark-blue text-[2.5rem]">
+                <div className="wrapper flex items-center justify-between flex-wrap-reverse lg:flex-nowrap">
+                    <div className="text-center lg:text-left lg:w-[430px] mx-auto lg:mx-0">
+                        <h1 className="text-dark-blue text-[2.5rem]  font-bold ">
                             Bring everyone
                             together to build
                             better products.
                         </h1>
-                        <p className="text-dark-blue opacity-50 my-4">
+                        <p className="my-4 text-different ">
                             Manage makes it simple for software teams to plan
                             day-to-day tasks while keeping the larger team goals in view.
                         </p>
                         <button className="get-started mt-2">Get Started</button>
                     </div>
-                    <Image className="md:mx-auto" src={heroImage} alt="hero image" />
+                    <Image className="mx-auto lg:mx-0" src={heroImage} alt="hero image" />
                 </div>
             </section>
             <section className="section-gap">
-                <div className="wrapper flex ">
+                <div className="wrapper flex items-start flex-wrap lg:flex-nowrap">
                     <div className="text-center md:text-left">
                         <h2 className="section-header">What’s different about Manage?</h2>
                         <p className="text-different my-4">Manage provides all the functionality your team needs,
@@ -30,17 +39,26 @@ export default function Home() {
                     </div>
                     <div>
                         <ul className="mt-12">
-                            <li>
-                                <h4 className="list-header"><span className="num">01</span> Track company-wide progress</h4>
-                                <p className="text-different">See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.</p>
+                            <li className="list-item">
+                                <span className="num">01</span>
+                                <div>
+                                    <h4 className="list-header">  Track company-wide progress</h4>
+                                    <p className="text-different">See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.</p>
+                                </div>
                             </li>
-                            <li>
-                                <h4 className="list-header"><span className="num">02</span> Advanced built-in reports</h4>
-                                <p className="text-different">Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.</p>
+                            <li className="list-item">
+                                <span className="num">02</span>
+                                <div>
+                                    <h4 className="list-header">  Advanced built-in reports</h4>
+                                    <p className="text-different">Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.</p>
+                                </div>
                             </li>
-                            <li>
-                                <h4 className="list-header"><span className="num">03</span> Everything you need in one place</h4>
-                                <p className="text-different">Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.</p>
+                            <li className="list-item">
+                                <span className="num">03</span>
+                                <div>
+                                    <h4 className="list-header">  Everything you need in one place</h4>
+                                    <p className="text-different">Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.</p>
+                                </div>
                             </li>
                         </ul>
                     </div>
@@ -51,15 +69,59 @@ export default function Home() {
                 <h1>cursel</h1>
                 <button className="get-started">Get Started</button>
             </section>
-            <section className="section-gap">
-                <div className="wrapper">
-                    <h2>Simplify how your team works today.</h2>
-                    <button className="bg-white get-started ">Get Started</button>
+            <section className="section-gap bg-[#F3603C] py-[5em]">
+                <div className="wrapper flex flex-wrap  lg:text-left text-center justify-between">
+                    <h2 className="text-[2.4rem] font-[700] text-white">Simplify how your team works today.</h2>
+                    <button className="btn-white mt-8 lg:mt-0 lg:mx-0 mx-auto hover:opacity-95">Get Started</button>
                 </div>
             </section>
-            <footer>
-                <div className="wrapper">
-                    another time
+            <footer className="bg-[#1D1E25] text-white py-16">
+                <div className="wrapper flex justify-between items-center">
+                    <div className="flex flex-wrap gap-20">
+                        <Image src={logo} alt="logo" />
+                        <div className="flex gap-4">
+                            <Image src={facebook} alt="logo" />
+                            <Image src={youtube} alt="logo" />
+                            <Image src={twitter} alt="logo" />
+                            <Image src={pinterest} alt="logo" />
+                            <Image src={instagram} alt="logo" />
+                        </div>
+                    </div>
+                    <div>
+                        <ul className="flex gap-12 flex-wrap">
+                            <li>
+                                <Link href="#">Community</Link>
+                            </li>
+                            <li>
+                                <Link href="#">Pricing</Link>
+                            </li>
+
+                            <li>
+                                <Link href="#">Product</Link>
+                            </li>
+                            <li>
+                                <Link href="#">About Us</Link>
+                            </li>
+                            <li>
+                                <Link href="#">Careers</Link>
+                            </li>
+                            <li>
+                                <Link href="#">Community</Link>
+                            </li>
+
+                            <li>
+                                <Link href="#">Privacy Policy</Link>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <div className="flex flex-wrap gap-4">
+                        <input type="email" placeholder="updates in your inbox" className="rounded-full px-[1em] py-[.5em]" />
+                        <button className="py-[0.5em] px-[1em] rounded-full font-bold text-[1rem] bg-[#F3603C]">Go</button>
+                        <p className="hidden lg:block">Copyright 2020. All Rights Reserved</p>
+                    </div>
+
                 </div>
             </footer>
         </>
